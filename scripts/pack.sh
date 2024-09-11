58bbc930dc03460b7e55a8673261baa9cc989f7c
badgerson <mail@badgerson.com>
2024-09-11 17:31:25 +0200

    initial commit

---
+++ scripts/pack.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+filename='Larion-one-continent-1.0.0'
+# Just a quick script used to distribute the mod
+7z a "$1/${filename}.jar" data fabric.mod.json pack.mcmeta pack.png LICENSE.md
+# Zip extension = datapack (Mojang made it so jar files cannot be used as datapacks.. for some reason..)
+7z a "$1/${filename}.zip" data fabric.mod.json pack.mcmeta pack.png LICENSE.md
+
+echo "packed to $1"
