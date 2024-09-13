186a9bd7deb89bc72e3f557379cdb0a533c2c96c
badgerson <mail@badgerson.com>
2024-09-13 15:13:24 +0200

    bump version

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-filename='Larion-one-continent-1.1.1'
+filename='Larion-one-continent-1.2.0'
 # Just a quick script used to distribute the mod
 7z a "$1/${filename}.jar" data fabric.mod.json pack.mcmeta pack.png LICENSE.md
 # Zip extension = datapack (Mojang made it so jar files cannot be used as datapacks.. for some reason..)
